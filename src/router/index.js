945ef609ebf1ba7b5e3dd43546ef7b3ffe0dfe7e
leo <ln777764@gmail.com>
2026-01-14 16:34:54 +0700

    fix unauthenticated user page view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import { LocalStorage } from 'quasar'
 
 /*
  * If not building with SSR mode, you can
@@ -24,6 +25,26 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
+  })
+
+  // Simple auth guard: require auth for main app pages
+  Router.beforeEach((to, from, next) => {
+    const publicPaths = ['/loginpage', '/landing', '/signup']
+    const isPublic = publicPaths.includes(to.path)
+
+    const token = LocalStorage.getItem('token')
+    const isAuthenticated = !!token
+
+    if (!isPublic && !isAuthenticated) {
+      return next('/loginpage')
+    }
+
+    // If already authenticated and navigating to login/signup/landing, redirect to home
+    if (isAuthenticated && isPublic && to.path !== '/home') {
+      return next('/home')
+    }
+
+    return next()
   })
 
   return Router
